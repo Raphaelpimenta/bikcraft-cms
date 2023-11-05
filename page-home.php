4ac73d2638f1d364935c9e0d27e2aaca94129514
Raphael Pimenta <raphael.pimentagomes@hotmail.com>
2023-11-05 05:10:52 -0300

    ajuste de caminho

--- page-home.php
+++ page-home.php
@@ -12,7 +12,7 @@
 					<p>“não tenha nada em sua casa que você não considere útil ou acredita ser bonito”</p>
 					<cite>WILLIAM MORRIS</cite>
 				</blockquote>
-				<a href="produtos.html" class="btn">Orçamento</a>
+				<a href="/produtos/" class="btn">Orçamento</a>
 			</div>
 		</section>
 		
