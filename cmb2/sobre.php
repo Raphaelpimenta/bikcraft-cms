219e06cdb03d482a81c46fc08d1784511d89e62b
Raphael Pimenta <raphael.pimentagomes@hotmail.com>
2023-11-07 00:12:53 -0300

    adicionando campos editáveis na página sobre

--- cmb2/sobre.php
+++ cmb2/sobre.php
@@ -14,6 +14,48 @@ function cmb2_fields_sobre(){
     ]);
 
     $cmb->add_field([
+        'name' => 'Título qualidade',
+        'id' => 'titulo_qualidade',
+        'type' => 'text',
+    ]);
+
+    $cmb->add_field([
+        'name' => 'Logo Bikcraft',
+        'id' => 'logo_imagem',
+        'type' => 'file',
+        'options' => [
+            'url' => false
+        ],
+    ]);
+
+    //Campo do Loop
+    $item_qualidade = $cmb->add_field([
+        'name' => 'Item Qualidade',
+        'id' => 'item_qualidade',
+        'type' => 'group',
+        'repeatble' => true,
+        'options' => [
+            'sortable' => true,
+            'add_button' => 'Adicionar Qualidade',
+            'remove_button' => 'Remover Qualidade',
+        ],
+    ]);
+
+    //Loop item
+    $cmb->add_group_field($item_qualidade, [
+        'name' => 'Título item qualidade',
+        'id' => 'titulo_item_qualidade',
+        'type' => 'text',
+    ]);
+
+    //Loop item
+    $cmb->add_group_field($item_qualidade, [
+        'name' => 'Descrição item qualidade',
+        'id' => 'descricao_item_qualidade',
+        'type' => 'textarea_small',
+    ]);
+
+    $cmb->add_field([
         'name' => "Descrição acima do botão 'sobre'",
         'id' => 'chamar_sobre_btn',
         'type' => 'text',
--- inc/qualidade.php
+++ inc/qualidade.php
@@ -1,20 +1,16 @@
 <?php $sobre = get_page_by_title('sobre')->ID?>
 <section class="qualidade container">
-    <h2 class="subtitulo">Qualidade</h2>
-    <img src="img/bikcraft-qualidade.png" alt="Bikcraft">
+    <h2 class="subtitulo"><?php the_field('titulo_qualidade', $sobre)?></h2>
+    <!-- <img src="img/bikcraft-qualidade.png" alt="Bikcraft"> -->
+    <img src="<?php the_field('logo_imagem', $sobre)?>" alt="Bikcraft">
     <ul class="qualidade_lista">
+		<?php $item_qualidade = get_field('item_qualidade', $sobre); 
+		if(isset($item_qualidade)) { foreach($item_qualidade as $item) { ?>
     	<li class="grid-1-3">
-    		<h3>Durabilidade</h3>
-    		<p>Sólida como pedra, leve como o vento e resistente como o diamante, são nossos diferenciais.</p>
+    		<h3><?php echo $item['titulo_item_qualidade']; ?></h3>
+    		<p><?php echo $item['descricao_item_qualidade']; ?></p>
     	</li>
-    	<li class="grid-1-3">
-    		<h3>Design</h3>
-    		<p>Feitas sob medida para o melhor conforto e eficiência. Adaptamos a sua Bikcraft para o seu corpo.</p>
-    	</li>
-    	<li class="grid-1-3">
-    		<h3>Sustentabilidade</h3>
-    		<p>Além de ajudar a cuidar do meio ambiente, tirando carros da rua, toda a produção é sustentável.</p>
-    		</li>
+		<?php } } ?>
     </ul>
     <?php if(!is_page('sobre')) { ?>
     <div class="call">
