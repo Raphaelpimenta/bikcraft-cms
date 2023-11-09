57f81fbca736bce1f7174c32d5b9b7786e6e19db
Raphael Pimenta <raphael.pimentagomes@hotmail.com>
2023-11-09 03:28:14 -0300

    adicionando id na página contatos

---
+++ inc/redes-sociais.php
@@ -0,0 +1,8 @@
+<?php $contato = get_page_by_title('contato')->ID?>
+
+<ul>
+    <?php $redes_sociais = get_field('redes_sociais', $contato);
+    if(isset($redes_sociais)) { foreach($redes_sociais as $redes){ ?>
+    <li><a href="<?php echo $redes['link_redes'];?>" target="_blank"><img src="<?php echo $redes['imagem_redes']; ?>" alt="<?php echo $redes['nome_redes']?>"></a></li>
+    <?php } } ?>
+</ul>
