a3656faa14abcc6ecea5e19ecddd184ae49c2a3e
Raphael Pimenta <raphael.pimentagomes@hotmail.com>
2023-11-11 10:15:04 -0300

    incluindo custom field na página de single produtos

--- single-produtos.php
+++ single-produtos.php
@@ -8,23 +8,25 @@
 
 		<section class="container produto_item animar-interno">
 			<div class="grid-11">
-				<img src="img/produtos/bikcraft-passeio-1.jpg" alt="Bikcraft Passeio">
-				<h2>Passeio</h2>
+				<img src="<?php the_field('imagem_produtos1')?>img/produtos/bikcraft-passeio-1.jpg" alt="Bikcraft <?php the_title();?>">
+				<!-- <h2>Passeio</h2> -->
+				<h2><?php the_title();?></h2>
 			</div>
 			<div class="grid-5 produto_icone">
-				<img src="img/produtos/passeio.png" alt="Icone Passeio">
+				<img src="<?php the_field('icone_produtos')?>img/produtos/passeio.png" alt="Bikcraft <?php the_title();?>">
 			</div>
 			<div class="grid-8">
-				<img src="img/produtos/bikcraft-passeio-2.jpg" alt="Bikcraft Passeio">
+				<img src="<?php the_field('imagem_produtos2')?>img/produtos/bikcraft-passeio-2.jpg" alt="Bikcraft <?php the_title();?>">
 			</div>
 			<div class="grid-8 produto_info">
-				<p>Muito melhor do que passear pela orla a vidros fechados. A Bikcraft Passeio é uma bicicleta que une conforto e praticidade para o seu dia a dia. Você nunca mais vai querer saber de outra.</p>
+				<!-- <p>Muito melhor do que passear pela orla a vidros fechados. A Bikcraft Passeio é uma bicicleta que une conforto e praticidade para o seu dia a dia. Você nunca mais vai querer saber de outra.</p>
 				<ul>
 					<li>Conforto</li>
 					<li>Praticidade</li>
 					<li>Design</li>
 					<li>Versatilidade</li>
-				</ul>
+				</ul> -->
+                <?php the_content();?>
 			</div>
 		</section>
 
